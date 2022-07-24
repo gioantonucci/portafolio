48323f8e01d18dbf961d69ea6d3bf082b884f1c2
gioantonucci <gioantonucci@hotmail.com>
2022-07-24 17:51:55 -0300

    fixing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className=" font-Over bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
+    <section className="font-Over bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
+        <div className="container px-6 py-8 mx-auto">
+            
+
       <h1>Hola! 👋 Me llamo</h1>
       <h2 className="text-xl">Giovanna Antonucci.</h2>
       <h2 className="font-Over">Full stack developer 👩🏻‍💻</h2>
      
-    </div>
+
+               
+            
+        </div>
+    </section>
+  
   )
 }
 
