69208b3e3764ca684d8b1996c98f1a412a2bbe10
gioantonucci <gioantonucci@hotmail.com>
2022-07-24 17:47:22 -0300

    skills

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,44 +17,44 @@ function Projects() {
           </div>
         </div>
  
-          <div class="card card-side bg-base-100 shadow-xl mb-5">
+          <div className="card card-side bg-base-100 shadow-xl mb-5">
             <figure>
               <img className="w-52 h-72 " src={PI} alt="Proyecto individual" />
             </figure>
-            <div class="card-body">
-              <h2 class="card-title">DOGS APP</h2>
+            <div className="card-body">
+              <h2 className="card-title">DOGS APP</h2>
               <p>Proyectyo indiviudal para Henry Bootcamp. Single Page App que consume una API.</p>
-              <div class="card-actions justify-end">
+              <div className="card-actions justify-end">
               <a target="_blank" href='https://pi-dogs-gioantonucci.vercel.app/'>
-                <button class="btn btn-primary">LINK AL DEPLOY</button>
+                <button className="btn btn-primary">LINK AL DEPLOY</button>
                 </a>
               </div>
             </div>
           </div>
-          <div class="card card-side bg-base-100 shadow-xl mb-5">
+          <div className="card card-side bg-base-100 shadow-xl mb-5">
             <figure>
-            <img className="w-52 h-72 " src={PG} alt="Proyecto individual" />
+            <img className="w-52 h-72 " src={PG} alt="Proyecto grupal" />
             </figure>
-            <div class="card-body">
-              <h2 class="card-title">RGB STORE</h2>
+            <div className="card-body">
+              <h2 className="card-title">RGB STORE</h2>
               <p>Proyecto grupal para Henry Bootcamp. E-commerce de tecnología.</p>
-              <div class="card-actions justify-end">
+              <div className="card-actions justify-end">
               <a target="_blank" href='https://pg-rgb-store-three.vercel.app/'>
-                <button class="btn btn-primary">LINK AL DEPLOY</button>
+                <button className="btn btn-primary">LINK AL DEPLOY</button>
                 </a>
               </div>
             </div>
           </div>
-          <div class="card card-side bg-base-100 shadow-xl mb-5">
+          <div className="card card-side bg-base-100 shadow-xl mb-5">
             <figure>
-            <img className="w-52 h-72 " src={GIOFLIX} alt="Proyecto individual" />
+            <img className="w-52 h-72 " src={GIOFLIX} alt="Gioflix" />
             </figure>
-            <div class="card-body">
-              <h2 class="card-title">GIOFLIX</h2>
+            <div className="card-body">
+              <h2 className="card-title">GIOFLIX</h2>
               <p>Clon de Netflix.</p>
-              <div class="card-actions justify-end">
+              <div className="card-actions justify-end">
                 <a target="_blank" href='https://gioflix.vercel.app/'>
-                <button class="btn btn-primary">LINK AL DEPLOY</button>
+                <button className="btn btn-primary">LINK AL DEPLOY</button>
                 </a>
               </div>
             </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React from "react";
 
 function Skills() {
   return (
-    <div className="font-Over bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
-      
-    </div>
-  )
+    <section className="font-Over bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
+      <div className="container px-6 py-8 mx-auto">
+        <div className="items-center lg:flex">
+          <div className="lg:w-1/2">
+            <h2 className="text-3xl font-bold text-gray-800 dark:text-gray-100">
+              SKILLS
+            </h2>
+          </div>
+        </div>
+        <h2 className="text-3xl font-bold text-gray-800 dark:text-gray-100">
+        </h2>
+        <div className="flex mx-auto">
+          <div class="w-24 mr-2 rounded-xl">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+          </div>
+          <div class="w-24 mr-2 rounded-xl">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" />
+          </div>
+          <div class="w-24 mr-2 placeholder:rounded-xl">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" />
+          </div>
+          <div class="w-24 mr-2 rounded-xl">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
+          </div>
+          <div class="w-24 mr-2 rounded-xl">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" />
+          </div>
+          <div class="w-24 mr-2 rounded-xl">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" />
+          </div>
+          </div>
+          <div className="flex mx-auto">
+          <div class="w-24 mt-4 mr-2 rounded-xl">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" />
+          </div>
+          <div class="w-24 mr-2 rounded-xl">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg" />
+          </div>
+          <div class="w-24 mt-4 mr-2 placeholder:rounded-xl">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg" />
+          </div>
+          <div class="w-24 mt-4 mr-2 rounded-xl">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/less/less-plain-wordmark.svg" />
+          </div>
+          <div class="w-24  mt-4 mr-2 rounded-xl">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" />
+          </div>
+          <div class="w-24 mt-4 mr-2 rounded-xl">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original-wordmark.svg" />
+          </div>
+          </div> <div className="flex mx-auto">
+          <div class="w-24 mt-4 mr-2 rounded-xl">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original-wordmark.svg" />
+          </div>
+          <div class="w-24 mr-2 rounded-xl">
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" />
+          </div>
+       
+        
+
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Skills
+export default Skills;
