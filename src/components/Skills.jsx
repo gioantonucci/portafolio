a9b0053862e5ae71edd73a1775ca09ef22aab027
gioantonucci <gioantonucci@hotmail.com>
2022-07-27 20:49:59 -0300

    listo skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -18,18 +18,18 @@ function Skills() {
         <div className="px-5 py-24 mx-auto">
           <h1 className="text-4xl font-bold lg:ml-20 text-white">SKILLS</h1>
         </div>
-        <div className="flex flex-wrap container rounded opacity-75 p-5 bg-white justify-center lg:justify-between">
-          <DiJsBadge className="w-12 hover:text-gray-600 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 dark:text-gray-400" />
-          <DiHtml5 className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 dark:text-gray-400" />
-          <DiCss3 className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 dark:text-gray-400" />
-          <DiReact className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 dark:text-gray-400" />
-          <SiRedux className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 dark:text-gray-400" />
-          <DiPostgresql className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 dark:text-gray-400" />
-          <DiNodejsSmall className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 dark:text-gray-400" />
-          <DiDatabase className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 dark:text-gray-400" />
-          <DiGithubBadge className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 dark:text-gray-400" />
-          <SiSequelize className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 dark:text-gray-400" />
-         <SiPostman className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 dark:text-gray-400"/>
+        <div className="flex flex-wrap container rounded opacity-75 p-10 bg-white justify-center lg:justify-between">
+          <DiJsBadge className="w-12 hover:text-gray-600 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
+          <DiHtml5 className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
+          <DiCss3 className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
+          <DiReact className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
+          <SiRedux className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
+          <DiPostgresql className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
+          <DiNodejsSmall className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
+          <DiDatabase className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
+          <DiGithubBadge className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
+          <SiSequelize className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
+          <SiPostman className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
         </div>
       </div>
     </section>
