7cddc6c030551d2f250f55bba23eff3d64c6470d
gioantonucci <gioantonucci@hotmail.com>
2022-07-28 14:48:52 -0300

    ahora si

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function Header() {
   return (
-    <section className="font-Over bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
+    <section className="h-screen flex justify-center items-center font-Over bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
       <div className="container px-6 py-8 mx-auto">
         <h2
-          className="flex mt-10 flex-col 12xl justify-center items-center lg:text-8xl bg-cover text-6xl bg-clip-text bg-center uppercase text-transparent font-extrabold tracking-wide antialiased"
+          className="flex mt-10 flex-col 12xl justify-center items-center lg:text-9xl bg-cover text-6xl bg-clip-text bg-center uppercase text-transparent font-extrabold tracking-wide antialiased"
           style={{
             backgroundImage:
               "url(https://c.tenor.com/z_2DoGAqxpcAAAAC/ocean.gif)",
@@ -13,7 +13,7 @@ function Header() {
         >
           Giovanna Antonucci.
         </h2>
-        <h1 className="text-white flex justify-center text-3xl "> {`<Full Stack Developer />`}</h1>
+        <h1 className="text-white flex justify-center text-4xl "> {`<Full Stack Developer />`}</h1>
       </div>
     </section>
   );
