905a0a14cc8f8802c19d1f1238553489fc725cf7
gioantonucci <gioantonucci@hotmail.com>
2022-07-29 13:49:41 -0300

    tooltip skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -20,18 +20,42 @@ function Skills() {
             <FormattedMessage id="nav.skills" defaultMessage="SKILLS" />
           </h1>
         </div>
-        <div className="flex flex-wrap container rounded opacity-75 p-10 bg-white justify-center lg:justify-between">
-          <DiJsBadge className="w-12 hover:text-gray-600 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
-          <DiHtml5 className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
-          <DiCss3 className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
-          <DiReact className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
-          <SiRedux className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
-          <DiPostgresql className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
-          <DiNodejsSmall className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
-          <DiGithubBadge className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
-          <SiSequelize className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
-          <SiPostman className="hover:text-gray-600 w-12 h-12 mx-10 my-6 fill-current md:mx-12 lg:m-0 text-gray-400" />
-        </div>
+        <section className="p-6 container rounded opacity-75 bg-white justify-center">
+          <div className="container p-4 mx-auto text-center"></div>
+          <div className="container flex flex-wrap justify-center mx-auto dark:text-gray-400">
+            <div className="flex justify-center items-center tooltip tooltip-primary w-1/2 p-6  md:w-1/3 xl:w-1/4" data-tip="Javascript">
+              <DiJsBadge className="w-12 hover:text-gray-600 h-12  fill-current md:mx-12 lg:m-0 text-gray-400" />
+            </div>
+
+            <div className="flex justify-center tooltip tooltip-primary w-1/2 p-6 align-middle md:w-1/3 xl:w-1/4"  data-tip="HTML">
+              <DiHtml5 className="hover:text-gray-600 w-12 h-12  fill-current md:mx-12 lg:m-0 text-gray-400" />
+            </div>
+            <div className="flex justify-center tooltip tooltip-primary w-1/2 p-6 align-middle md:w-1/3 xl:w-1/4" data-tip="CSS">
+              <DiCss3 className="hover:text-gray-600 w-12 h-12  fill-current md:mx-12 lg:m-0 text-gray-400" />
+            </div>
+            <div className="flex justify-center tooltip tooltip-primary w-1/2 p-6 align-middle md:w-1/3 xl:w-1/4" data-tip="ReactJs">
+              <DiReact className="hover:text-gray-600 w-12 h-12 fill-current md:mx-12 lg:m-0 text-gray-400" />
+            </div>
+            <div className="flex justify-center tooltip tooltip-primary w-1/2 p-6 align-middle md:w-1/3 xl:w-1/4" data-tip="Redux">
+              <SiRedux className="hover:text-gray-600 w-12 h-12 fill-current md:mx-12 lg:m-0 text-gray-400" />
+            </div>
+            <div className="flex justify-center tooltip tooltip-primary w-1/2 p-6 align-middle md:w-1/3 xl:w-1/4" data-tip="PostgreSQL">
+              <DiPostgresql className="hover:text-gray-600 w-12 h-12  fill-current md:mx-12 lg:m-0 text-gray-400" />
+            </div>
+            <div className="flex justify-center tooltip tooltip-primary w-1/2 p-6 align-middle md:w-1/3 xl:w-1/4" data-tip="NodeJs">
+              <DiNodejsSmall className="hover:text-gray-600 w-12 h-12  fill-current md:mx-12 lg:m-0 text-gray-400" />
+            </div>
+            <div className="flex justify-center tooltip tooltip-primary w-1/2 p-6 align-middle md:w-1/3 xl:w-1/4" data-tip="Sequelize">
+              <SiSequelize className="hover:text-gray-600 w-12 h-12  fill-current md:mx-12 lg:m-0 text-gray-400" />
+            </div>
+            <div className="flex justify-center tooltip tooltip-primary w-1/2 p-6 align-middle md:w-1/3 xl:w-1/4" data-tip="GitHub">
+              <DiGithubBadge className="hover:text-gray-600 w-12 h-12  fill-current  text-gray-400" />
+            </div>
+            <div className="flex justify-center tooltip tooltip-primary  w-1/2 p-6 align-middle md:w-1/3 xl:w-1/4" data-tip="Postman">
+              <SiPostman className="hover:text-gray-600 w-12 h-12 fill-current md:mx-12 lg:m-0 text-gray-400"  />
+            </div>
+          </div>
+        </section>
       </div>
     </section>
   );
