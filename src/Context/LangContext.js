a8e8e2966064045a4d8d7d00c9f7db285c3a5d72
gioantonucci <gioantonucci@hotmail.com>
2022-07-29 03:21:34 -0300

    localstorage

--- src/Context/LangContext.js
+++ src/Context/LangContext.js
@@ -6,6 +6,23 @@ import { IntlProvider } from "react-intl";
 const langContext = React.createContext();
 
 const LangProvider = ({ children }) => {
+  let localeDefault;
+  let mensajeDefault;
+  const lang = localStorage.getItem("lang");
+  if (lang) {
+    localeDefault = lang
+    if (lang === "es-ES") {
+      mensajeDefault = MensajesEspañol;
+    } else if (lang === "en-US") {
+      mensajeDefault = MensajesIngles;
+    } else {
+      localeDefault = 'en-US'
+      mensajeDefault = MensajesIngles
+    }
+  }
+
+
+  
   const [mensajes, setMensajes] = useState("MensajesEspañol");
   const [locale, setLocale] = useState("es-ES");
 
@@ -14,23 +31,26 @@ const LangProvider = ({ children }) => {
       case "es-ES":
         setMensajes(MensajesEspañol);
         setLocale("es-ES");
+        localStorage.setItem("lang", "es-ES");
         break;
       case "en-US":
         setMensajes(MensajesIngles);
         setLocale("en-US");
+        localStorage.setItem("lang", "en-US");
         break;
       default:
         setMensajes(MensajesEspañol);
         setLocale("es-ES");
+        localStorage.setItem("lang", "es-ES");
     }
   };
   return (
     <langContext.Provider value={{ setLang: setLang, locale: locale }}>
       <IntlProvider locale={locale} messages={mensajes}>
         {children}
-    </IntlProvider>
+      </IntlProvider>
     </langContext.Provider>
   );
 };
 
-export { LangProvider, langContext};
+export { LangProvider, langContext };
