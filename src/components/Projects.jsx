f4303d474ae515a5c7b36e0d7e9cbfbee594fa19
gioantonucci <gioantonucci@hotmail.com>
2022-08-18 17:07:40 -0300

    update projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import PI from "../images/PIDOGS.png";
 import PG from "../images/PGRGB.png";
 import GIOFLIX from "../images/GIOFLIX.png";
-import PF from "../images/PORTFOLIO.jpg"
+import PF from "../images/PORTFOLIO.jpg";
+import AVATAR from "../images/AVATAR.jpg"
 import { FormattedMessage } from "react-intl";
 
 function Projects() {
-
-  
-
-
   return (
     <section className="font-Over  bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
       <div className="conteiner lg:mx-20 px-5 py-24">
         <div className="flex flex-wrap w-full mb-10">
           <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
-          <h1 className="text-4xl font-bold uppercase mb-12 text-teal-50"> <FormattedMessage id="nav.projects" defaultMessage="PROYECTOS" /></h1>
+            <h1 className="text-4xl font-bold uppercase mb-12 text-teal-50">
+              {" "}
+              <FormattedMessage id="nav.projects" defaultMessage="PROYECTOS" />
+            </h1>
           </div>
         </div>
 
@@ -24,11 +24,14 @@ function Projects() {
             <img className="w-fit h-52" src={PI} alt="Proyecto individual" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title uppercase"><FormattedMessage
-            id="title.dogs" defaultMessage="DOGS APP"/></h2>
+            <h2 className="card-title uppercase">
+              <FormattedMessage id="title.dogs" defaultMessage="DOGS APP" />
+            </h2>
             <p>
-            <FormattedMessage
-            id="description.dogs" defaultMessage="Proyecto individual para Henry Bootcamp. Aplicación de una página que consume una API. Hecha con JS, HTML, CSS, React, Redux."/>
+              <FormattedMessage
+                id="description.dogs"
+                defaultMessage="Proyecto individual para Henry Bootcamp. Aplicación de una página que consume una API. Hecha con JS, HTML, CSS, React, Redux."
+              />
             </p>
             <div className="card-actions justify-end">
               <a
@@ -36,8 +39,12 @@ function Projects() {
                 rel="noopener noreferrer"
                 href="https://pi-dogs-gioantonucci.vercel.app/"
               >
-                <button className="btn btn-primary uppercase"><FormattedMessage
-            id="button.deploy" defaultMessage="DEPLOY"/></button>
+                <button className="btn btn-primary uppercase">
+                  <FormattedMessage
+                    id="button.deploy"
+                    defaultMessage="DEPLOY"
+                  />
+                </button>
               </a>
             </div>
           </div>
@@ -47,16 +54,27 @@ function Projects() {
             <img className="h-52 w-fit" src={PG} alt="Proyecto grupal" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title uppercase"><FormattedMessage
-            id="title.rgb" defaultMessage="RGB STORE"/></h2>
+            <h2 className="card-title uppercase">
+              <FormattedMessage id="title.rgb" defaultMessage="RGB STORE" />
+            </h2>
             <p>
-            <FormattedMessage
-            id="description.rgb" defaultMessage="Proyecto grupal para Henry Bootcamp. E-commerce de tecnología. Hecha con JS, HTML, Tailwind, React, Redux, NodeMailer, Passportjs, auth0, Nodejs, PostgreeSQL entre otras."/>
+              <FormattedMessage
+                id="description.rgb"
+                defaultMessage="Proyecto grupal para Henry Bootcamp. E-commerce de tecnología. Hecha con JS, HTML, Tailwind, React, Redux, NodeMailer, Passportjs, auth0, Nodejs, PostgreeSQL entre otras."
+              />
             </p>
             <div className="card-actions justify-end">
-              <a target="_blank" rel="noopener noreferrer" href="https://pg-rgb-store-three.vercel.app/">
-                <button className="btn btn-primary"><FormattedMessage
-            id="button.deploy" defaultMessage="DEPLOY"/></button>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://pg-rgb-store-three.vercel.app/"
+              >
+                <button className="btn btn-primary">
+                  <FormattedMessage
+                    id="button.deploy"
+                    defaultMessage="DEPLOY"
+                  />
+                </button>
               </a>
             </div>
           </div>
@@ -67,12 +85,24 @@ function Projects() {
           </figure>
           <div className="card-body">
             <h2 className="card-title uppercase">GIOFLIX</h2>
-            <p><FormattedMessage
-            id="description.gioflix" defaultMessage="Clon de Netflix. Realizado para practicar tecnologías."/></p>
+            <p>
+              <FormattedMessage
+                id="description.gioflix"
+                defaultMessage="Clon de Netflix. Realizado para practicar tecnologías."
+              />
+            </p>
             <div className="card-actions justify-end">
-              <a target="_blank" rel="noopener noreferrer" href="https://gioflix.vercel.app/">
-                <button className="btn btn-primary"><FormattedMessage
-            id="button.deploy" defaultMessage="DEPLOY"/></button>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://gioflix.vercel.app/"
+              >
+                <button className="btn btn-primary">
+                  <FormattedMessage
+                    id="button.deploy"
+                    defaultMessage="DEPLOY"
+                  />
+                </button>
               </a>
             </div>
           </div>
@@ -82,15 +112,52 @@ function Projects() {
             <img className="w-fit h-52" src={PF} alt="Proyecto grupal" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title uppercase"><FormattedMessage
-            id="title.portfolio" defaultMessage="PORTFOLIO"/></h2>
+            <h2 className="card-title uppercase">
+              <FormattedMessage
+                id="title.portfolio"
+                defaultMessage="PORTFOLIO"
+              />
+            </h2>
             <p>
-            <FormattedMessage
-            id="description.portfolio" defaultMessage="Página personal dedicada a mostrar proyectos y skills."/>
+              <FormattedMessage
+                id="description.portfolio"
+                defaultMessage="Página personal dedicada a mostrar proyectos y skills."
+              />
             </p>
             <div className="card-actions justify-end">
-              <a target="_blank" rel="noopener noreferrer" href="https://gioantonucci.vercel.app/">
-                <button className="btn btn-primary"><FormattedMessage
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://gioantonucci.vercel.app/"
+              >
+                <button className="btn btn-primary">
+                  <FormattedMessage
+                    id="button.deploy"
+                    defaultMessage="DEPLOY"
+                  />
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="card mb-5 bg-opacity-75 lg:card-side bg-base-100 shadow-xl">
+          <figure>
+            <img className="w-fit h-52" src={AVATAR} alt="Juego de Avatar" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title uppercase"><FormattedMessage
+            id="title.avatar" defaultMessage="AVATAR GAME"/></h2>
+            <p>
+            <FormattedMessage
+            id="description.avatar" defaultMessage="Juego del estilo piedra, papel o tijera. Realizado para reforzar conocimientos y buenas prácticas. Hecho con JS, HTML y CSS."/>
+            </p>
+            <div className="card-actions justify-end">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://avatar-game.vercel.app/"
+              >
+                <button className="btn btn-primary uppercase"><FormattedMessage
             id="button.deploy" defaultMessage="DEPLOY"/></button>
               </a>
             </div>
